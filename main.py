a948d5ac6b2045541564ae72dbe7b717f062aecf
Schuka48 <artem.shchukin.2000@mail.ru>
2022-02-04 18:50:15 +0300

    learning functions in Python

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
-import random
-#
-# aliens = []
-# colors = ['red', 'green', 'blue', 'yellow']
-#
-# speeds = ['slow', 'medium', 'fast']
-#
-# for i in range(0, 25):
-#     alien = {}
-#     points = random.randint(0, 100)
-#     color_int = random.randint(0, 3)
-#     speed = random.randint(0, 2)
-#     alien['color'] = colors[color_int]
-#     alien['points'] = points
-#     alien['speed'] = speeds[speed]
-#     # print(f'{i + 1}. {alien}')
-#     aliens.append(alien)
-#
-# for i, alien in enumerate(aliens[:5]):
-#     print(i + 1)
-#     for key, value in alien.items():
-#         print(f'{key}: {value}')
-#     print("-" * 30)
-#
-# print(f'Total number of aliens: {len(aliens)}')
+from typing import Dict, Any
 
-favorite_languages = {
-    'jen': ['python', 'ruby'],
-    'sarah': ['c'],
-    'edward': ['ruby', 'go'],
-    'phil': ['python', 'haskell'],
-    }
+users = []
 
-for name, languages in favorite_languages.items():
-    if len(languages) == 1:
-        print(f"\n{name.title()}'s favorite languages are: {languages[0].title()}")
-    else:
-        print(f"\n{name.title()}'s favorite languages are:")
-        for language in languages:
-            print(f"\t{language.title()}")
+
+def add_user(user_name: str, user_age: int = 23) -> dict:
+    user: Dict[str, int] = {'user_name': user_name,
+            'user_age': user_age
+            }
+    return user
+
+
+message = "Input Name and Age of added person: "
+text = ''
+while 'q' not in text:
+    user = {}
+    text = input(message)
+    if 'q' in text:
+        break
+    name, age = text.split(' ')[0], int(text.split(' ')[1])
+    users.append(add_user(name, age))
+
+print(users)
