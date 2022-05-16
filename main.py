883c0354ad590cf4bb1a301644c00c256e1f7e3f
Schuka48 <artem.shchukin.2000@mail.ru>
2022-05-16 22:24:44 +0300

    First Commit

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-from typing import Dict, Any
+import socket
+import threading
+import struct
 
-users = []
-
-
-def add_user(user_name: str, user_age: int = 23) -> dict:
-    user: Dict[str, int] = {'user_name': user_name,
-            'user_age': user_age
-            }
-    return user
-
-
-message = "Input Name and Age of added person: "
-text = ''
-while 'q' not in text:
-    user = {}
-    text = input(message)
-    if 'q' in text:
-        break
-    name, age = text.split(' ')[0], int(text.split(' ')[1])
-    users.append(add_user(name, age))
-
-print(users)
+print("Start!")
